32873e3778a3c25710f67c71eb1894e1724c38e7
paulashby <paul@primitive.co>
2017-03-18 15:25:54 +0000

    User overrides added for menu links

--- wp-content/themes/harmonics/game-engine.php
+++ wp-content/themes/harmonics/game-engine.php
@@ -7,6 +7,11 @@
 		<?php wp_head(); ?>				
 	</head>	
 	<?php 
+	function get_url_by_slug($slug) {
+	    $page_url_id = get_page_by_path( $slug );
+	    $page_url_link = get_permalink($page_url_id);
+	    return esc_url( $page_url_link );
+	};
 	echo "<body  data-db='" . esc_url( site_url() . '/?page_id=6' ) . "' data-starturl='" . esc_url( content_url() . '/startPage/src/index.html' ) . "' data-templateurl='" . get_template_directory_uri() . "' data-timeoutduration='" . get_field('inactivity-timeout-duration', 'option') . "'id='bodyElmt'>"; ?>
 			<div id='menuContainer' class='hideMenu'>
 		
@@ -37,43 +42,73 @@
 
 				    	echo "<ul>
 					        <li class='logoutBttn'>";				        	
-				        	$ServicesPageName = get_field('services-page-name', $currUser);
-							$redirectURL = esc_url( content_url(). '/servicespages/' . $ServicesPageName );
+				        	$linkPageName = get_field('services-page-name', $currUser);
+							$redirectURL = esc_url( content_url(). '/servicesPages/' . $linkPageName );
 							$redirectString = '[logout redirect="' . $redirectURL . '"]';
+							$gamesPageURL = get_url_by_slug( 'game-engine' );
 				        	echo do_shortcode( $redirectString ) . "</li>
 			        		<li class='exitBttn exitGame'><a href=''><img src='" . esc_url( get_template_directory_uri() )  . "/css/img/menu_exitgame.png' alt='exit game' data-category='exit'></a></li>
 				        	<li class='teamBttn changeTeams'><a href=''><img src='" . esc_url( get_template_directory_uri() ) . "/css/img/menu_changeteams.png' alt='change teams' data-category='teamchange'></a></li>
-				        	<li class='gamesBttn games'><a href='" . esc_url( get_home_url() ) . "'><img src='" . esc_url( get_template_directory_uri() ) . "/css/img/menu_games.png' alt='games'></a></li>";
+				        	<li class='gamesBttn games'><a href='" . $gamesPageURL . "'><img src='" . esc_url( get_template_directory_uri() ) . "/css/img/menu_games.png' alt='games'></a></li>";
 
 				        while( have_rows("menu-items", "option") ) {
 				        	
 				        	the_row();
 				        	$title = get_sub_field("title");
-				        	$linkURL = get_sub_field("link-url");
+				        	$linkURL = "";
 
-				        	// Check for URL overrides entered on user page
-				        	if( have_rows("user-menu-items", $currUser)){
-
-							    while( have_rows("user-menu-items", $currUser)){
-
-							    	the_row();
-							        
-							        $linkTitle = get_sub_field("user-menu-items-title");
-							        
-							        // Use user URL if available
-							        if($linkTitle == $title) {
-							        	$linkURL = get_sub_field("link-url");
-							        }
-							    }
-							}
-							// Only include item in menu if include is checked on Game Engine Settings page
 				        	if(get_sub_field("include")){
-				        		 if($title == "services") {
-				        			$ServicesPageName = get_field('services-page-name', $currUser);
-            						$linkURL = esc_url( content_url(). '/servicespages/' . $ServicesPageName );
-				        		}
-				        		echo "<li class='" . $title . "'><a href='" . $linkURL . "'><img src='" . esc_url( get_template_directory_uri() . "/css/img/menu_" . $title . '.png' ) . "' alt='" . $title . "' data-category='" . $title . "'></a></li>";	
-				        	}			        				        	
+					        	switch ( $title ) {
+					        		case "services":
+					        		// Check for user override					        		
+					        		$linkPageName = get_field('services-page-name', $currUser);
+					        			
+					        		if( strlen( $linkPageName ) == 0 ) {
+					        			// If override unavailable, use Game Engine settings
+					        			$linkURL = esc_url( content_url(). '/servicesPages/' . get_sub_field("page-name") );
+					        		} else {
+					        			$linkURL = esc_url( content_url(). '/servicesPages/' . $linkPageName );
+					        		}				        		
+	        						break;
+
+	        						case "cardgames":
+					        		// Check for user override
+	        						$linkPageName = get_field('card-games-page-name', $currUser);
+					        		
+					        		if( strlen( $linkPageName ) == 0 ) {
+					        			// If override unavailable, use Game Engine settings
+					        			$linkURL = esc_url( content_url(). '/appsCategoryPages/' . get_sub_field("page-name") );
+					        		} else {
+					        			$linkURL = esc_url( content_url(). '/appsCategoryPages/' . $linkPageName );
+					        		}
+	        						break;
+
+	        						case "shopping":
+					        		// Check for user override 
+	        						$linkPageName = get_field('shopping-page-name', $currUser);
+					        		
+					        		if( strlen( $linkPageName ) == 0 ) {
+					        			// If override unavailable, use Game Engine settings
+					        			$linkURL = esc_url( content_url(). '/appsCategoryPages/' . get_sub_field("page-name") );
+					        		} else {
+					        			$linkURL = esc_url( content_url(). '/appsCategoryPages/' . $linkPageName );
+					        		}
+	        						break;
+
+	        						default:
+					        		// Check for user override 
+	        						$linkPageName = get_field('whiteboard-page-name', $currUser);	        						
+					        		
+					        		if( strlen( $linkPageName ) == 0 ) {
+					        			// If override unavailable, use Game Engine settings
+					        			$linkURL = esc_url( content_url(). '/appsCategoryPages/' . get_sub_field("page-name") );
+					        		} else {
+					        			$linkURL = esc_url( content_url(). '/appsCategoryPages/' . $linkPageName );	
+					        		}
+					        	}					        	
+					        	echo "<li class='" . $title . "'><a href='" . $linkURL . "'><img src='" . esc_url( get_template_directory_uri() . "/css/img/menu_" . $title . '.png' ) . "' alt='" . $title . "' data-category='" . $title . "'></a></li>";	
+				        	
+					        }
 				        }
 				        echo "</ul>
 				        </div>";
