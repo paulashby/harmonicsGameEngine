71775b84e3615ae49e80ddb362825d1723bd52b5
paulashby <paul@primitive.co>
2017-04-24 13:39:03 +0100

    Redirects inactive login screen to services page for current machine

---
+++ wp-content/themes/harmonics/scripts/loginTimeout.js
@@ -0,0 +1,31 @@
+/*global document, top, clearTimeout, localStorage, setTimeout */
+var loginTimeout = (function () {	
+
+	'use strict';
+
+	var 
+	INACTIVITY_PERIOD = localStorage.getItem('inactivityTimeout'),
+	homeURL = localStorage.getItem('homeURL'),
+	inactivityTimeout,
+	onTimeout,
+	resetTimer;
+
+	// INACTIVITY_PERIOD & homeURL will be available from first time 
+	// machine loads games page until the browser cache is cleared
+    if(INACTIVITY_PERIOD && homeURL) {
+    	onTimeout = function () {
+		  top.location.href = homeURL;
+		};
+		resetTimer = function (e) {
+		  if(e && e.type !== 'keypress') {
+		    e.preventDefault();
+		  }
+		  clearTimeout(inactivityTimeout);    
+		  inactivityTimeout = setTimeout(onTimeout, INACTIVITY_PERIOD);
+		}; 
+		document.addEventListener("click", resetTimer, false);
+		document.addEventListener("touchend", resetTimer, false);
+		document.addEventListener("keypress", resetTimer, false);
+		resetTimer();   	
+    }
+}());
