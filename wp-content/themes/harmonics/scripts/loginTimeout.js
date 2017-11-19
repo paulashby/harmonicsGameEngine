a30130ebd8c8302cf438a2d2e7c7a3dc79308d8f
paulashby <paul@primitive.co>
2017-11-19 13:31:54 +0000

    Guards against short timeout, sets timeout page to generic static Services pages
    Unless Services is disabled, in which case it reloads login page

--- wp-content/themes/harmonics/scripts/loginTimeout.js
+++ wp-content/themes/harmonics/scripts/loginTimeout.js
@@ -1,28 +1,36 @@
 /*global document, top, clearTimeout, localStorage, setTimeout */
 var loginTimeout = (function () {	
 
+	/*
+	 * Sets the timeout for the login page - once INACTIVITY_PERIOD has expired, homeURL will be loaded
+	*/
+
 	'use strict';
 
 	var 
+	MIN_INACTIVITY_PERIOD = 15000, // Prevent ridiculously short timeout
 	INACTIVITY_PERIOD = localStorage.getItem('inactivityTimeout'),
 	homeURL = localStorage.getItem('homeURL'),
+	loginTimeoutURL = localStorage.getItem('loginTimeoutURL'),
 	inactivityTimeout,
 	onTimeout,
 	resetTimer;
 
-	// INACTIVITY_PERIOD & homeURL will be available from first time 
-	// machine loads games page until the browser cache is cleared
-    if(INACTIVITY_PERIOD && homeURL) {
-    	onTimeout = function () {
-		  top.location.href = homeURL;
-		};
-		resetTimer = function (e) {		  
-		  clearTimeout(inactivityTimeout);    
-		  inactivityTimeout = setTimeout(onTimeout, INACTIVITY_PERIOD);
-		}; 
-		document.addEventListener("click", resetTimer, false);
-		document.addEventListener("touchend", resetTimer, false);
-		document.addEventListener("keypress", resetTimer, false);
-		resetTimer();   	
+	if(INACTIVITY_PERIOD && homeURL) {
+
+    	INACTIVITY_PERIOD = Math.max(MIN_INACTIVITY_PERIOD, parseInt(INACTIVITY_PERIOD, 10));    	
+
+    		onTimeout = function () {
+    			var timeoutURL = loginTimeoutURL || window.location.href;
+    			top.location.href = timeoutURL;
+			};
+			resetTimer = function (e) {		  
+			  clearTimeout(inactivityTimeout);
+			  inactivityTimeout = setTimeout(onTimeout, INACTIVITY_PERIOD);
+			}; 
+			document.addEventListener("click", resetTimer, false);
+			document.addEventListener("touchend", resetTimer, false);
+			document.addEventListener("keypress", resetTimer, false);
+			resetTimer();    	    	   	
     }
 }());
