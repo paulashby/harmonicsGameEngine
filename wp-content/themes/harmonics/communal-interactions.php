52ea0c36c4362be36d9aba528c46d95fd0c01346
paulashby <paul@primitive.co>
2018-10-12 09:52:44 +0100

    update communal animation state on current user's communal group

--- wp-content/themes/harmonics/communal-interactions.php
+++ wp-content/themes/harmonics/communal-interactions.php
@@ -4,16 +4,19 @@
 	write_log( date('[Y-m-d H:i e] '). 'Communal interactions requesting ' . $reqType . ' animation' );
 	if($reqType) {
 
-		$currUser ="user_" . get_current_user_id();
+		$currUsrID = get_current_user_id();
 
+		// Communal Group for current user
+		$comgroup = get_field_object('communal-group-select', 'user_' . $currUsrID);
+		$communalGroup = $comgroup['value'];
+		$groupID = $communalGroup->ID;
+
+		// $communalGroup is a post with fields for hearts, bubbles etc
 		switch($reqType) {
 			case "heart":		
-			$hearts = get_field('hearts', $currUser);
+			$hearts = get_field('hearts', $groupID);
 			$newVal = $hearts + 1;
-			update_field('hearts', $newVal, $currUser);
-		}
-		
+			update_field('hearts', $newVal, $groupID);
+		}		
 	}
-
-
 ?>
