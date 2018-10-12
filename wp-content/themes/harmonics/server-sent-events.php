cac83a4bd939fa2e03adef02a765b1933bab0836
paulashby <paul@primitive.co>
2018-10-12 10:02:55 +0100

    interact with communal animation state stored on current user's communal group

--- wp-content/themes/harmonics/server-sent-events.php
+++ wp-content/themes/harmonics/server-sent-events.php
@@ -4,13 +4,20 @@
 	header('Cache-Control: no-cache');
 
 	function getRequiredAnimations() {
-		
-		$currUser ="user_" . get_current_user_id();
-		$hearts = get_field('hearts', $currUser);
-		// $newVal = $hearts > 0 ? $hearts - 1 : 0;
-		update_field('hearts', 0, $currUser);
 
-		return $hearts;
+		$currUsrID = get_current_user_id();
+
+		// Communal Group for current user
+		$comgroup = get_field_object('communal-group-select', 'user_' . $currUsrID);
+		$communalGroup = $comgroup['value'];
+		$groupID = $communalGroup->ID;
+
+		// $communalGroup is a post with fields for hearts, bubbles etc
+		$hearts = get_field('hearts', $groupID);
+		update_field('hearts', 0, $groupID);
+
+		return $hearts;		
+
 	}
 	$data = getRequiredAnimations();
 	echo "data: " . $data . "\n\nretry: 250\n\n";
